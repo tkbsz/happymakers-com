57b6c11ce0db44abb24bc112d83b96732235e91d
rolf.schenk@mac.com <rolf.schenk@mac.com@1688cff8-7bbf-11de-ae39-777466e4ac8d>
2010-08-14 23:13:24 +0000

    test2

--- trunk/SecureVaadinApplicationDemo/src/java/demoapp/security/UserRoles.java
+++
@@ -1,9 +0,0 @@
-package demoapp.security;
-
-public class UserRoles {
-
-	public static final String ROLE_DIRECTOR = "DIRECTOR";
-	public static final String ROLE_PAYROLL_ASSISTANT = "PAYROLL_ASSISTANT";
-	public static final String ROLE_PROJECT_MANAGER = "PROJECT_MANAGER";
-	
-}
